d785f581a67a220d9acd8beacb2b20280783ba0e
GBM1986 <gunnar_bjarni@hotmail.com>
2023-08-28 12:02:43 +0200

    Opgave

---
+++ assets/main.js
@@ -0,0 +1,18 @@
+let name = "Gunnar"
+
+
+let age = 37;
+let number2 = 5;
+
+let sum = age + number2;
+
+let myNumber = 1;
+myNumber++;
+myNumber--;
+
+console.log(myNumber);
+
+
+console.log("the sum is " + sum);
+
+console.log(name);
